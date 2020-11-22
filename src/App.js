cc984a2bf6402f5027231f3ba3dbfecad7e25664
Leo <adamwang1209@gmail.com>
2020-11-22 22:48:33 +0800

    v1.0

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import React, { Children } from 'react';
+import './App.css';
+
+function ListItem({ listItem }) {
+    return (<>
+        <div > {`- ${listItem}`} </div>
+    </>
+    )
+}
+
+function App() {
+    let [value, setValue] = React.useState("")
+    let [list, setList] = React.useState([])
+
+    let handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(value);
+        if (value) {
+            setList([...list, value])
+        }
+    }
+
+    let inputChangeHandle = (e) => {
+        // console.log(e.target.value)
+        setValue(e.target.value)
+    }
+
+    let deleteClick = (deleteItemID) => () => {
+        // console.log(deleteItemID)
+        setList(
+            list.filter((item, index) =>
+                index != deleteItemID
+            )
+        )
+    }
+    let done = (doneID) => () => {
+
+    }
+
+    React.useEffect(() => {
+        //console.log(list);
+    }, [list])
+
+    return (
+        <>
+            <form onSubmit={handleSubmit} >
+                <
+                    input type='text'
+                    value={value}
+                    onChange={inputChangeHandle}
+                />
+                <button type='submit'> add </button> </form >
+
+            {
+                list.map((item, index) =>
+                    <div style={
+                        {
+                            width: 250,
+                            display: "flex",
+                            justifyContent: 'space-between'
+                        }
+                    }
+                        key={index} >
+                        <ListItem listItem={item} > </ListItem>
+                        <div>
+                            <span onClick={deleteClick(index)} > Delete </span>
+                            <span onClick={done(index)} > Done </span>
+                        </div>
+                    </div>
+                )
+            }
+
+        </>
+    )
+
+}
+
+export default App;
